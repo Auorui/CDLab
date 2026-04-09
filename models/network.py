3380f3cadf0695303216fd580570082c9b2386b2
夏天是冰红茶 <100789256+Auorui@users.noreply.github.com>
2026-04-09 09:33:02 +0800

    Add files via upload

--- models/network.py
+++ models/network.py
@@ -1,4 +1,16 @@
-from .baseline import *
+"""
+MSCANet (A CNN-Transformer Network With Multiscale Context Aggregation for Fine-Grained Cropland Change Detection)
+MeGNet  (A Memory-Guided Network and a Novel Dataset for Cropland Semantic Change Detection)
+STNet   (STNet: Spatial and Temporal feature fusion network for change detection in remote sensing images)
+SNUNet  (SNUNet-CD: A Densely Connected Siamese Network for Change Detection of VHR Images)
+SiamUnet_diff (Fully Convolutional Siamese Networks for Change Detection)
+ChangeFormer  (A Transformer-Based Siamese Network for Change Detection)
+DCSI_UNet  (A Dual-Stream UNet With Parallel Channel–Spatial Interaction and Aggregation for Change Detection)
+LENet      (A Remote Sensing Image Change Detection Method Integrating Layer-Exchange and Channel-Spatial Differences)
+ISDANet    (Interactive and Supervised Dual-Mode Attention Network for Remote Sensing Image Change Detection)
+
+"""
+from models.baseline import *
 
 MODEL_CLASSES = {
     "MSCANet"          : MSCANet,
@@ -9,6 +21,7 @@ MODEL_CLASSES = {
     "ChangeFormer"     : ChangeFormerV6,
     "DCSI_UNet"        : DCSI_UNet,
     "LENet"            : LENet,
+    "ISDANet"          : ISDANet,
 }
 
 def get_change_networks(name, **kwargs):
@@ -19,7 +32,20 @@ def get_change_networks(name, **kwargs):
         raise ValueError(f"Model {name} not found in MODEL_CLASSES")
 
 
+if __name__ == "__main__":
+    import torch
+    from utils.utils import load_config
+    x1 = torch.rand(1, 3, 256, 256).cuda()
+    x2 = torch.rand(1, 3, 256, 256).cuda()
+    conf = load_config('E:\PythonProject\CDLab\config\MeGNet.yaml')
+    Net = get_change_networks(conf.model.name, **conf.model.params).cuda()
 
+    # Calculate GFLOPs & Parameters / 计算 FLOPs 与参数量
+    from thop import profile
+
+    flops, params = profile(Net, inputs=(x1, x2))
+    print(f"Model FLOPs: {flops / 1e9:.4f} GFLOPs")
+    print(f"Model Parameters: {params / 1e6:.2f} M")
 
 
 
